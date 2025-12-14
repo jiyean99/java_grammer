0ce4501b7606295ab6dad12677b67f09846a4110
Jiyean Lee <117697228+jiyean99@users.noreply.github.com>
2025-12-14 20:26:06 +0900

    C01Bacis 디렉터리의 C11Map.java 시트 내 백준-베스트셀러 제출 코드 추가

--- src/C01Basic/C11Map.java
+++ src/C01Basic/C11Map.java
@@ -220,6 +220,53 @@ public class C11Map {
 
         // [📝실습예제] 백준 - 베스트셀러
         // https://www.acmicpc.net/problem/1302
+        /*
+        import java.io.*;
+        import java.util.*;
+
+        public class Main {
+            public static void main(String[] args) throws IOException {
+                // n : 들어온 책의 개수
+                // input : 책 이름 * 5
+                // output : 가장 많이 팔린 책의 제목 출력
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+                int n = Integer.parseInt(br.readLine());
+
+                // TODO 책 이름과 수를 담는 map
+                Map<String, Integer> my_map = new HashMap<>();
+
+                for (int i = 0; i < n; i++){
+                    String name = br.readLine();
+                    if (!my_map.containsKey(name)) {
+                        my_map.put(name, 1);
+                    }else {
+                        my_map.put(name, my_map.get(name) + 1);
+                    }
+                }
+
+                int max = Integer.MIN_VALUE;
+                String max_book_name = "";
+
+                List<String> my_list = new ArrayList<>();
+
+                for (String a : my_map.keySet()) {
+                    if (max < my_map.get(a)){
+                        max = my_map.get(a);
+                    }
+                }
+                for (String a : my_map.keySet()) {
+                    if(my_map.get(a) == max){
+                        my_list.add(a);
+                    }
+                }
+                Collections.sort(my_list);
+
+                System.out.println(my_list.get(0));
+            }
+        }
+        */
+
 
         // TODO 숙제
         // [📝실습예제] 백준 - 파일정리(TreeMap)
